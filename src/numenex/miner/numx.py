4ee25fa495db299f04ca12b81a98ea5fd1f08dc9
wall-e <w411-e@users.noreply.github.com>
2024-08-11 02:12:31 +0545

    feat: updated subnet for UI compatibility

--- src/numenex/miner/numx.py
+++ src/numenex/miner/numx.py
@@ -1,40 +1,24 @@
 from ..numenex import NumenexQAModule
 from ..settings import Role
-import argparse
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import typing as ty
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Miner system for QA")
-    parser.add_argument(
-        "question_answer_function",
-        type=str,
-        choices=["get_questions", "answer_questions"],
-        help="The function to execute",
-    )
     numenex_module = NumenexQAModule(role=Role.Miner)
-    if parser.parse_args().question_answer_function == "get_questions":
-        numenex_module.get_questions(path="questions")
-    elif parser.parse_args().question_answer_function == "answer_questions":
-        data = [
-            {
-                "answer": "string",
-                "question_id": "b44c07f6-d9f7-4ce0-9516-1d235eea0eb5",
-                "supporting_resources": {},
-            }
-        ]
-        answers = [
-            {
-                "question_id": "26e34ba9-4e0d-46ab-8f8e-820fce3f5e76",
-                "answer": "answer 1 of 1",
-                "supporting_resources": {},
-            },
-            {
-                "question_id": "1d990f08-77f7-4278-ae1c-5b1bb5232be7",
-                "answer": "answer 1 of 2",
-                "supporting_resources": {},
-            },
-        ]
-        numenex_module.answer_questions(method="post", data=answers, path="answers")
+    server = FastAPI()
+    server.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"])
+    server.add_api_route("/questions/", numenex_module.get_questions, methods=["GET"])
+
+    @server.post("/answers/")
+    async def answer_questions(answers: ty.List[ty.Dict[str, ty.Any]]):
+        return numenex_module.answer_questions(
+            data=answers, method="post", path="answers"
+        )
+
+    uvicorn.run(server, host="0.0.0.0", port=9000)
 
 
 if __name__ == "__main__":
--- src/numenex/validator/numx.py
+++ src/numenex/validator/numx.py
@@ -5,63 +5,78 @@ from communex._common import get_node_url
 from communex.compat.key import classic_load_key
 from communex.client import CommuneClient
 import argparse
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import typing as ty
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Validator system for QA")
-    parser.add_argument(
-        "question_answer_function",
-        type=str,
-        choices=["get_answers", "validate_and_update_answers"],
-        help="The function to execute",
-    )
+    # parser = argparse.ArgumentParser(description="Validator system for QA")
+    # parser.add_argument(
+    #     "question_answer_function",
+    #     type=str,
+    #     choices=["get_answers", "validate_and_update_answers"],
+    #     help="The function to execute",
+    # )
+    # numenex_module = NumenexQAModule(role=Role.Validator)
+    # if parser.parse_args().question_answer_function == "get_answers":
+    #     numenex_module.get_answers(path="answers")
+    # elif parser.parse_args().question_answer_function == "validate_and_update_answers":
+    #     config = Config(Role.Validator)
+    #     use_testnet: Annotated[bool, "Whether to use testnet or not"] = (
+    #         config["subnet"]["use_testnet"] == "True"
+    #     )
+    #     c_client = CommuneClient(get_node_url(use_testnet=use_testnet))
+    #     keypair = classic_load_key(config["validator"]["key"])
+    #     max_allowed_weights = int(config["subnet"]["max_allowed_weights"])
+    #     answers = [
+    #         {
+    #             "id": "1fa6f43c-73cc-401c-9544-763ea9799d51",
+    #             "created_at": "2024-08-06T01:31:54.847839+05:45",
+    #             "updated_at": "2024-08-06T01:31:54.847839+05:45",
+    #             "answer": "answer 1 of 1",
+    #             "question_id": "26e34ba9-4e0d-46ab-8f8e-820fce3f5e76",
+    #             "supporting_resources": {},
+    #             "miner": {
+    #                 "id": "f665a3e3-b9f5-40dd-b606-7905432c1253",
+    #                 "created_at": "2024-08-06T01:31:54.827930+05:45",
+    #                 "updated_at": "2024-08-06T01:31:54.827930+05:45",
+    #                 "user_address": "5FLbaLYazG4EnJdz2pw2ZEgaSYRfwQN7FqufcV4xtW1RVitm",
+    #                 "user_type": "miner",
+    #                 "module_id": 1,
+    #             },
+    #             "score": 0.5,
+    #         },
+    #         {
+    #             "id": "e6f4d4d9-0d2b-46d6-82fc-26ce7965233b",
+    #             "created_at": "2024-08-06T01:31:54.847839+05:45",
+    #             "updated_at": "2024-08-06T01:31:54.847839+05:45",
+    #             "answer": "answer 1 of 2",
+    #             "question_id": "1d990f08-77f7-4278-ae1c-5b1bb5232be7",
+    #             "supporting_resources": {},
+    #             "miner": {
+    #                 "id": "f665a3e3-b9f5-40dd-b606-7905432c1253",
+    #                 "created_at": "2024-08-06T01:31:54.827930+05:45",
+    #                 "updated_at": "2024-08-06T01:31:54.827930+05:45",
+    #                 "user_address": "5FLbaLYazG4EnJdz2pw2ZEgaSYRfwQN7FqufcV4xtW1RVitm",
+    #                 "user_type": "miner",
+    #                 "module_id": 1,
+    #             },
+    #             "score": 0.1,
+    #         },
+    #     ]
+
+    #     numenex_module.answer_questions(
+    #         method="patch", data=formatted_answer_validations, path="answers"
+    #     )
     numenex_module = NumenexQAModule(role=Role.Validator)
-    if parser.parse_args().question_answer_function == "get_answers":
-        numenex_module.get_answers(path="answers")
-    elif parser.parse_args().question_answer_function == "validate_and_update_answers":
-        config = Config(Role.Validator)
-        use_testnet: Annotated[bool, "Whether to use testnet or not"] = (
-            config["subnet"]["use_testnet"] == "True"
-        )
-        c_client = CommuneClient(get_node_url(use_testnet=use_testnet))
-        keypair = classic_load_key(config["validator"]["key"])
-        max_allowed_weights = int(config["subnet"]["max_allowed_weights"])
-        answers = [
-            {
-                "id": "1fa6f43c-73cc-401c-9544-763ea9799d51",
-                "created_at": "2024-08-06T01:31:54.847839+05:45",
-                "updated_at": "2024-08-06T01:31:54.847839+05:45",
-                "answer": "answer 1 of 1",
-                "question_id": "26e34ba9-4e0d-46ab-8f8e-820fce3f5e76",
-                "supporting_resources": {},
-                "miner": {
-                    "id": "f665a3e3-b9f5-40dd-b606-7905432c1253",
-                    "created_at": "2024-08-06T01:31:54.827930+05:45",
-                    "updated_at": "2024-08-06T01:31:54.827930+05:45",
-                    "user_address": "5FLbaLYazG4EnJdz2pw2ZEgaSYRfwQN7FqufcV4xtW1RVitm",
-                    "user_type": "miner",
-                    "module_id": 1,
-                },
-                "score": 0.5,
-            },
-            {
-                "id": "e6f4d4d9-0d2b-46d6-82fc-26ce7965233b",
-                "created_at": "2024-08-06T01:31:54.847839+05:45",
-                "updated_at": "2024-08-06T01:31:54.847839+05:45",
-                "answer": "answer 1 of 2",
-                "question_id": "1d990f08-77f7-4278-ae1c-5b1bb5232be7",
-                "supporting_resources": {},
-                "miner": {
-                    "id": "f665a3e3-b9f5-40dd-b606-7905432c1253",
-                    "created_at": "2024-08-06T01:31:54.827930+05:45",
-                    "updated_at": "2024-08-06T01:31:54.827930+05:45",
-                    "user_address": "5FLbaLYazG4EnJdz2pw2ZEgaSYRfwQN7FqufcV4xtW1RVitm",
-                    "user_type": "miner",
-                    "module_id": 1,
-                },
-                "score": 0.1,
-            },
-        ]
+    server = FastAPI()
+    server.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"])
+    server.add_api_route("/answers/", numenex_module.get_answers, methods=["GET"])
+
+    @server.patch("/answers/")
+    async def validate_and_update_answers(answers: ty.List[ty.Dict[str, ty.Any]]):
         score_dict = {}
         for answer in answers:
             module_id = answer["miner"]["module_id"]
@@ -70,18 +85,15 @@ def main():
             score_dict[answer["miner"]["module_id"]]["score"] += answer["score"]
         numenex_module.set_weights(
             score_dict=score_dict,
-            netuid=config["subnet"]["netuid"],
-            client=c_client,
-            key=keypair,
-            max_allowed_weights=max_allowed_weights,
         )
         formatted_answer_validations = [
             {"id": answer["id"], "score": answer["score"]} for answer in answers
         ]
-        print(formatted_answer_validations)
-        numenex_module.answer_questions(
-            method="patch", data=formatted_answer_validations, path="answers"
+        return numenex_module.answer_questions(
+            data=answers, method="patch", path="answers"
         )
+
+    uvicorn.run(server, host="0.0.0.0", port=9000)
 
 
 if __name__ == "__main__":
