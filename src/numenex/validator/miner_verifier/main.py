b7bb7faf45e097104b0e58708724c6254f5852c1
wall-e <w411-e@users.noreply.github.com>
2024-10-13 11:43:38 +0545

    fix: api ninja calls

--- src/numenex/validator/miner_verifier/main.py
+++ src/numenex/validator/miner_verifier/main.py
@@ -18,14 +18,14 @@ def get_result(
         config["llm_validator"]["openai_timeout"],
         config["llm_validator"]["openai_max_retries"],
     )
-    for url in miner_submission["supporting_resources"].values():
-        if not get_website_metadata(url, config["llm_validator"]["api_ninja_api_key"]):
-            return {
-                "validation_result": "invalid",
-                "justification": "The website is not reliable",
-                "reference_snippet": "",
-                "score": 0.0,
-            }
+    # for url in miner_submission["supporting_resources"].values():
+    #     if not get_website_metadata(url, config["llm_validator"]["api_ninja_api_key"]):
+    #         return {
+    #             "validation_result": "invalid",
+    #             "justification": "The website is not reliable",
+    #             "reference_snippet": "",
+    #             "score": 0.0,
+    #         }
     docs = get_website_contents(miner_submission["supporting_resources"].values())
     chain = final_prompt | llm
     result = chain.invoke(
