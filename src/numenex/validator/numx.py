8a067ba04f58b1c9b0cf37d713019f380dca277a
wall-e <w411-e@users.noreply.github.com>
2025-01-02 20:31:32 +0545

    feat: vali erroring out and separate log files

--- src/numenex/validator/numx.py
+++ src/numenex/validator/numx.py
@@ -12,16 +12,48 @@ from logging.handlers import RotatingFileHandler
 ANSWER_JSON = "validated_answers.json"
 IP_REGEX = re.compile(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+")
 
+# check for logs folder if not found create one
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[
-        RotatingFileHandler("app.log", maxBytes=1000000, backupCount=5),
+        RotatingFileHandler("logs/app.log", maxBytes=1000000, backupCount=5),
         logging.StreamHandler(),
     ],
 )
+
 logger = logging.getLogger(__name__)
+# Create separate handlers for different log levels
+info_handler = RotatingFileHandler("logs/info.log", maxBytes=1000000, backupCount=5)
+info_handler.setLevel(logging.INFO)  # Only INFO and above will be logged here
+info_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+)
+critical_handler = RotatingFileHandler(
+    "logs/critical.log", maxBytes=1000000, backupCount=5
+)
+critical_handler.setLevel(
+    logging.CRITICAL
+)  # Only Critical and above will be logged here
+critical_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+)
+
+error_handler = RotatingFileHandler("logs/error.log", maxBytes=1000000, backupCount=5)
+error_handler.setLevel(logging.ERROR)  # Only ERROR and above will be logged here
+error_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+)
+
+# Add the new handlers to the logger
+logger.addHandler(info_handler)
+logger.addHandler(error_handler)
+logger.addHandler(critical_handler)
+
 
 os.environ["USER_AGENT"] = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
@@ -87,52 +119,55 @@ def main():
             f"validator key {numenex_module.keypair.ss58_address} is not registered in subnet"
         )
     while True:
-        answers = numenex_module.get_answers(path="answers")
-        processed_answers, unprocessed_answers, score_dict = get_unprocessed_answers(
-            answers, swapped_modules_keys
-        )
-        config = Config(Role.Validator)
-        if len(answers) == 0:
-            logger.info("No Miners to validate")
-        else:
-            for answer in unprocessed_answers:
-                if len(answer["supporting_resources"]) == 0:
-                    answer["score"] = 0
-                else:
-                    try:
-                        result = get_result(answer, config)
-                        logger.info({"result": result, "answer": answer})
-                        answer["score"] = float(result["score"])
-                    except Exception:
-                        logger.error("Error while validating answer", exc_info=True)
-                        continue
-                module_id = swapped_modules_keys[answer["miner"]["user_address"]]
-                if not module_id:
-                    logger.error(
-                        f"Could not find module_id for user_address: {answer['miner']['user_address']}"
-                    )
-                    continue
-                if module_id not in score_dict:
-                    score_dict[module_id] = {"score": 0}
-                score_dict[module_id]["score"] += answer["score"]
-                processed_answers.append(
-                    {
-                        "id": answer["id"],
-                        "score": answer["score"],
-                        "module_id": answer["miner"]["module_id"],
-                    }
-                )
-            save_validated_answers(processed_answers)
-            numenex_module.set_weights(score_dict=score_dict)
-            formatted_answer_validations = [
-                {"id": answer["id"], "score": answer["score"]}
-                for answer in unprocessed_answers
-            ]
-            numenex_module.answer_questions(
-                data=formatted_answer_validations, method="patch", path="answers"
+        try:
+            answers = numenex_module.get_answers(path="answers")
+            processed_answers, unprocessed_answers, score_dict = (
+                get_unprocessed_answers(answers, swapped_modules_keys)
             )
-        logger.info("Sleeping for %s seconds", config["validator"]["interval"])
-        time.sleep(int(config["validator"]["interval"]))
+            config = Config(Role.Validator)
+            if len(answers) == 0:
+                logger.info("No Miners to validate")
+            else:
+                for answer in unprocessed_answers:
+                    if len(answer["supporting_resources"]) == 0:
+                        answer["score"] = 0
+                    else:
+                        try:
+                            result = get_result(answer, config)
+                            logger.info({"result": result, "answer": answer})
+                            answer["score"] = float(result["score"])
+                        except Exception:
+                            logger.error("Error while validating answer", exc_info=True)
+                            continue
+                    module_id = swapped_modules_keys[answer["miner"]["user_address"]]
+                    if not module_id:
+                        logger.error(
+                            f"Could not find module_id for user_address: {answer['miner']['user_address']}"
+                        )
+                        continue
+                    if module_id not in score_dict:
+                        score_dict[module_id] = {"score": 0}
+                    score_dict[module_id]["score"] += answer["score"]
+                    processed_answers.append(
+                        {
+                            "id": answer["id"],
+                            "score": answer["score"],
+                            "module_id": answer["miner"]["module_id"],
+                        }
+                    )
+                save_validated_answers(processed_answers)
+                numenex_module.set_weights(score_dict=score_dict)
+                formatted_answer_validations = [
+                    {"id": answer["id"], "score": answer["score"]}
+                    for answer in unprocessed_answers
+                ]
+                numenex_module.answer_questions(
+                    data=formatted_answer_validations, method="patch", path="answers"
+                )
+            logger.info("Sleeping for %s seconds", config["validator"]["interval"])
+            time.sleep(int(config["validator"]["interval"]))
+        except Exception as e:
+            logger.critical(f"Error in running vali {e}", exc_info=True)
 
 
 if __name__ == "__main__":
